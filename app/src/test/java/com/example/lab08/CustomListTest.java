41f973e023ed33a7119bcc85631abab4fa3fe43d
Basma Ebeid <basma.ebeid123@gmail.com>
2025-10-28 16:24:14 -0600

    Failed test no hasCity() yet

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 // random comment to test git actions
+@Test
+void hasCity_false_then_true_after_add() {
+    CustomList list = new CustomList();
+    City edm = new City("Edmonton", "AB");
+    assertFalse(list.hasCity(edm));   // should fail now because method doesn’t exist yet
+    list.addCity(edm);
+    assertTrue(list.hasCity(edm));
+}
+
 }
